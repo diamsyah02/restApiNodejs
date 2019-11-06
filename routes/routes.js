1349d95feaf705381094a87288c0d6c10736bf3b
diamsyah02 <38798124+diamsyah02@users.noreply.github.com>
2019-11-06 18:34:02 +0700

    update upload image

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,39 @@
 'use strict';
-module.exports = function(app){
+module.exports = function (app) {
     let users = require('../controllers/users');
     let auth = require('../controllers/auth');
     let middleware = require('../middleware/users');
+    var multer = require('multer');
+
+    var storage = multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, './assets/images');
+        },
+        filename: (req, file, cb) => {
+            console.log(file);
+            var filetype = '';
+            if (file.mimetype === 'image/gif') {
+                filetype = 'gif';
+            }
+            if (file.mimetype === 'image/png') {
+                filetype = 'png';
+            }
+            if (file.mimetype === 'image/jpeg') {
+                filetype = 'jpg';
+            }
+            cb(null, 'image-' + Date.now() + '.' + filetype);
+        }
+    });
+    var upload = multer({ storage: storage });
+
+    // upload
+    app.route('/upload').post(upload.single('file'),function(req, res, next) {
+        if(!req.file) {
+          res.status(500).send({error: true, message: 'Upload unsuccessfully !'});
+          return next(err);
+        }
+        res.status(200).send({ error: false, file: req.file, message: 'Upload successfully !' });
+      });
 
     // route user
     app.route('/users')
